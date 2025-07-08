7a8a84e901a1cea8c9363d8374a7d4d8397b3d9d
Vincent <vincent00lav@outlook.com>
2025-07-08 17:15:13 +0200

    Faire défiler avec les doigts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  ActivityIndicator,
+  ScrollView,
+  Switch,
+  Text,
+  View,
+} from "react-native";
+import styles from "./styles";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView style={styles.scroll}>
+        {new Array(20).fill(null).map((v, i) => (
+          <View key={i}>
+            <Text style={[styles.scrollItem, styles.text]}>Some text</Text>
+            <ActivityIndicator style={styles.scrollItem} size="large" />
+            <Switch style={styles.scrollItem} />
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
